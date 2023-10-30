291b1a7fa233cc7cc63ae0ea464f68555f33bde0
Keeners Navarro//KINGROCK2042 <130202458+KINGROCK2005@users.noreply.github.com>
2023-10-30 17:36:13 -0600

    Add files via upload
    Introduccion a POO sesion del dia lunes 30 de octubre de 2023

---
+++ introPOO.java/src/App.java
@@ -0,0 +1,8 @@
+import java.util.SCanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        PC miPC = new PC(); //Creando un objeto de tipo PC
+                            //Instalando un obbjeto en la clase
+        Scanner lector = new Scanner(System.in);
+    }
+}
